edf637465eb0d0f6143daee8a6ac3d16a4a4e53e
psycopros <proooosgameeees122@gmail.com>
2024-05-19 16:03:31 +0700

    lesson 1

---
+++ (1)_Javascript_introduction/script.js
@@ -0,0 +1,12 @@
+
+//console
+console.log("Goodbye world");
+console.log("This is my second line");
+
+//alert thingy
+// window.alert("Welcome!");
+
+document.getElementById("title").textContent = " Goodbye world";
+document.getElementById("text").textContent = "damn this too easy im so bored atleast this is good or something";
+
+
