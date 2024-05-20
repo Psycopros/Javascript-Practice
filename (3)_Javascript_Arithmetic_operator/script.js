6170c53901f6f3e5dd4dc9412d780c28ad7c8341
psycopros <proooosgameeees122@gmail.com>
2024-05-20 18:49:01 +0700

    lesson 3

---
+++ (3)_Javascript_Arithmetic_operator/script.js
@@ -0,0 +1,29 @@
+let items = 20;
+
+// - plus, minus, multipl, devide, modulous -
+// items = items + 1;
+// items = items - 1;
+// items = items * 2;
+// items = items / 2;
+// items = items ** 2;
+// items = items % 3;
+
+// - fast way with same variavble -
+// items += 1;
+// items -= 1;
+// items *= 2;
+// items /= 2;
+// items **= 2;
+// items %= 3;
+
+console.log(items)
+
+// math problem
+
+x = 34 + 9 / 3 - 24;
+y = 50 % 3 - 34 * (45 - 53);
+z = 84 - 21 * 3 / (32 * 3)
+
+console.log(x);
+console.log(y);
+console.log(z);
