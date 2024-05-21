c688d5f3b6f0ce6ff727951f421e27e2560250c4
psycopros <proooosgameeees122@gmail.com>
2024-05-21 19:30:34 +0700

    lesson 5

---
+++ (5)_Javascript_type_conversion/script.js
@@ -0,0 +1,20 @@
+// input change type
+
+// let age = window.prompt("Please Type Your Age : ");
+
+// age = Number(age)
+// age += 1;
+
+// console.log(age)
+
+let x = "5";
+let y = "something";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x , typeof(x));
+console.log(y , typeof(y));
+console.log(z , typeof(z));
