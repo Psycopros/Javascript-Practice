be967dfdd8d91e2f618aa6a13cd418bc8694cbff
psycopros <proooosgameeees122@gmail.com>
2024-05-23 17:23:53 +0700

    lesson 8

---
+++ (8)_Javascript_math_object/script.js
@@ -0,0 +1,25 @@
+let x = 9;
+let y = 2.521;
+let z = 7;
+
+let a = 90;
+
+console.log(`PI = ${Math.PI}`);
+console.log(`euler number = ${Math.E} `);
+console.log(`round number of ${y} = ${Math.round(y)} `);
+console.log(`round up number of ${y} = ${Math.ceil(y)} `);
+console.log(`round down number of ${y} = ${Math.floor(y)} `);
+console.log(`remove decimal number of ${y} = ${Math.trunc(y)} `);
+console.log(`power number of ${x} ^ ${z} = ${Math.pow(x, z)} `);
+console.log(`round number of ${y} = ${Math.round(y)} `);
+
+console.log(`sin of ${a} = ${Math.sin(a)} `);
+console.log(`cos of ${a} = ${Math.cos(a)} `);
+console.log(`tan of ${a} = ${Math.tan(a)} `);
+
+console.log(`check value state of 10 = ${Math.sign(10)} `);
+console.log(`check value state of -10 = ${Math.sign(-10)} `);
+console.log(`check value state of 1-0 = ${Math.sign(0)} `);
+
+console.log(`find smallest value from ${x}, ${y}, ${z} = ${Math.min(x, y, z)} `);
+console.log(`find biggest value from ${x}, ${y}, ${z} = ${Math.max(x, y, z)} `);
