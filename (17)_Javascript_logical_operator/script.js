774a8d602caf990d29ea8cbc4229fa381e60a442
psycopros <proooosgameeees122@gmail.com>
2024-05-29 22:59:56 +0700

    lesson 17

--- (17)_Javascript_logical_operator/script.js
+++ (17)_Javascript_logical_operator/script.js
@@ -1,9 +1,16 @@
-let celcius = 30;
+let celcius = 26;
+let cookMinutes = 45; 
 
-// and logical operatr &&
-
+// and logical operator &&
 if(celcius >0 && celcius <=30){
     console.log("Today weather is good");
 } else{
-    console("Today weather is bad")
+    console.log("Today weather is bad");
+}
+
+// and logical operatr ||
+if(cookMinutes <=50 || cookMinutes >=75){
+    console.log("Failed! Your food is may undercook or overcook!");
+} else{
+    console.log("Sucess! your food well cooked");
 }
