1a68e38f800307c8284fe738dd34b0b0d029f52e
psycopros <proooosgameeees122@gmail.com>
2024-05-30 22:11:08 +0700

    lesson 18

---
+++ (18)_Javascript_strict_equality/script.js
@@ -0,0 +1,26 @@
+let number = "123";
+const PI = 3.14;
+
+if(number == 123){
+    console.log("Yes the number are equal");
+} else {
+    console.log("NO the number aren't equal");
+}
+
+if(number === 123){
+    console.log("Yes the number are equal");
+} else {
+    console.log("NO the number aren't equal");
+}
+
+if(PI != 3.14){
+    console.log("NO the Pi value aren't equal");
+} else {
+    console.log("Yes the PI value are equal");
+}
+
+if(PI !== "3.14"){
+    console.log("NO the Pi value aren't equal");
+} else {
+    console.log("Yes the PI value are equal");
+}
